698a103dd0f144ad4a23caeaf687a24ebb7dbec6
William Lyon <wlyon@quantpost.com>
2013-11-03 14:22:07 -0700

    update server.js

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-var express = require('express');
-var app = express();
+"use strict"
 
+var express = require('express'),
+  hbs = require('hbs');
+
+var app = express();
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
 app.use(express.logger());
 
 app.get('/', function(req, res){
-	res.send('Yo Dawg');
+  res.render('index_template', {});
 });
 
+
 var port = process.env.PORT || 5000;
-app.listen(port, function(){
-	console.log("Listening on " + port);
-});
+
+
+app.listen(port, function() {
+  console.log("listening on " + port);
+})
